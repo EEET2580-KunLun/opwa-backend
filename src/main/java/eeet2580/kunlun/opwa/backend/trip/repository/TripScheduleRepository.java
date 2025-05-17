7a2ed5d35526e8dfc831deca96fe0eedb558e8ff
Duc Le <dealoux@gmail.com>
2025-05-17 13:38:45 +0700

    remove dup

--- src/main/java/eeet2580/kunlun/opwa/backend/trip/repository/TripScheduleRepository.java
+++ src/main/java/eeet2580/kunlun/opwa/backend/trip/repository/TripScheduleRepository.java
@@ -3,7 +3,6 @@ package eeet2580.kunlun.opwa.backend.trip.repository;
 import eeet2580.kunlun.opwa.backend.trip.model.TripScheduleEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,7 +14,6 @@ import java.util.List;
 public interface TripScheduleRepository extends MongoRepository<TripScheduleEntity, String> {
     List<TripScheduleEntity> findByLineId(String lineId);
 
-    List<TripScheduleEntity> findByLineId(String lineId, Pageable pageable);
     long countByLineId(String lineId);
 
     Page<TripScheduleEntity> findByLineId(String lineId, Pageable pageable);
